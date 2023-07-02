5ce051a7643d70a308023b73c44baad60d056eae
Zachary Bucknor-Smartt <zacharybsmartt@gmail.com>
2023-07-02 16:48:15 -0500

    structure

--- Unit 1 OOP/1.4.1 - 1.4.11 section 1.py
+++ Unit 1 OOP/1.4.1 - 1.4.11 section 1.py
@@ -37,4 +37,23 @@ sumAbsProd([2, -3], [4, -5])
 
 # need more list comprehension practice
 
+# 1.4.7 optional
+def isPalindrome(string):
+    forward_i = 0
+    backwards_i = -1
+    for i in string:
+        if string[forward_i] == string[backwards_i]:
+            forward_i += 1
+            backwards_i -= 1
+        
+        else:
+            return False
+
+    return True
+
+isPalindrome("racecar")
+
+# 1.4.8 substring optional
+def isSubstring(str1, str2):
+    
 
