1b0f4e13d8605346783b07ef0935925b4f590206
Zachary Bucknor-Smartt <zacharybsmartt@gmail.com>
2023-07-07 13:43:38 -0500

    Recitation 3 Update

--- UNIT 1 State Machines/Recitation 3 Notables.py
+++ UNIT 1 State Machines/Recitation 3 Notables.py
@@ -6,3 +6,9 @@ Lambdas allow us to start talking to python about functions not previously defin
 demolist = [1, 2, 4, 5]
 demolist = list(map(lambda x : x * 2, demolist))
 print(demolist)
+
+#List comprehensions (Haskal)
+listComprehension = [(x, y) for x in demolist for y in demolist]
+print(listComprehension)
+
+# stuff you already knew, watch out for aliasing
